42ab43655fc2bd978c95b4e77dd719b8279bd1b2
German Gallo Poblano <galloger@gmail.com>
2026-04-04 14:18:08 -0600

    cambio de rama a main

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import Card from "./components/Card.js";
 import Section from "./components/Section.js";
+import Popup from "./components/Popup.js";
 import PopupWithImage from "./components/PopupWithImage.js";
 import PopupWithForm from "./components/PopupWithForm.js";
 import UserInfo from "./components/UserInfo.js";
